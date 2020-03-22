4322b9c68ea5944935ff56a1eaf58c0d613de9a0
Louis <woodylouis@users.noreply.github.com>
2020-03-22 23:26:15 +0800

    adding validator

--- reduxlogin/server/routes/users.js
+++ reduxlogin/server/routes/users.js
@@ -1,9 +1,40 @@
 import express from 'express';
+import isEmpty from 'lodash/isEmpty';
+import validator from 'validator';
 
 let router = express.Router();
 
+const validateInput = (data) => {
+  let errors = {};
+
+  if (validator.isEmpty(data.username)) {
+    errors.username = "The field is required";
+  }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = "The field is required";
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = "The field is required";
+  }
+
+  if (validator.isEmpty(data.passwordConfirmation)) {
+    errors.passwordConfirmation = "The field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
+
 router.post('/', (req, res) => {
-  console.log(req.body)
+  const { errors, isValid } = validateInput(req.body);
+
+  if (!isValid) {
+    res.status(400).json(errors);
+  }
 });
 
 export default router;
